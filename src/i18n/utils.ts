23459837886a8d10db10d477364dc32bc70c406c
Fabrizio <fabrinucci99@gmail.com>
2023-11-01 10:56:10 -0300

    Add i18n

---
+++ src/i18n/utils.ts
@@ -0,0 +1,25 @@
+import { ui, defaultLang, showDefaultLang } from './ui'
+
+export function getLangFromUrl(url: URL) {
+  const [, lang] = url.pathname.split('/')
+
+  if (lang in ui) return lang as keyof typeof ui
+  return defaultLang
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key]
+  }
+}
+
+export function getTranslations(lang: keyof typeof ui) {
+  const l = ui[lang]
+  return ui[lang]
+}
+
+export function useTranslatedPath(lang: keyof typeof ui) {
+  return function translatePath(path: string, l: string = lang) {
+    return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`
+  }
+}
