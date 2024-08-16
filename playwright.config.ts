7a0acc90ce1d0caa8e585c9eb2b4230e8a1f9a52
Fabrizio <fabrinucci99@gmail.com>
2024-08-16 14:48:33 -0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: 'npm run start',
+    command: 'npm run dev',
     url: 'http://127.0.0.1:4321',
     reuseExistingServer: !process.env.CI
   }
